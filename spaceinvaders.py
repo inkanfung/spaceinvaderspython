69467cadbd162c54a515e3a68f63b2373a2272d2
inkanfung <inkan1998@gmail.com>
2018-09-02 13:45:00 +0100

    added main character

--- spaceinvaders.py
+++ spaceinvaders.py
@@ -5,7 +5,7 @@ import turtle
 
 #setting up screen
 wn = turtle.Screen()
-wn.bgcolour("black")
+wn.bgcolor("black")
 wn.title("Space Invaders")
 
 #draw border
@@ -15,21 +15,21 @@ border.color("white")
 
 #setting up position
 border.penup()
-border.setposition(-50,-50)
+border.setposition(-300,-300)
 border.pendown()
 border.pensize(3)
 
 #drawing square
 for side in range(4):
-    border.fd(50)
-    border.lt(5)
+    border.fd(600)
+    border.lt(90)
 border.hideturtle()
 
 #create the player turtle
 player = turtle.Turtle()
 player.color("red")
-player.shape("circle")
+player.shape("triangle")
 player.penup()
 player.speed(0)
-player.setposition(0, -20)
-player.sethead(90)
+player.setposition(0, -250)
+player.setheading(90)
